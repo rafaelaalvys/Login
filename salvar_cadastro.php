fb712ec0459b54d6bc97c09935b38acffa97a47d
claudiocardo <rafaela.alvys2020@outlook.com>
2025-12-02 21:41:40 -0300

    php

--- salvar_cadastro.php
+++ salvar_cadastro.php
@@ -2,4 +2,7 @@
 //Recebemos aqui os dados enviados pelo formulário
 $nome = $_POST['nome'];
 $email = $_POST['email'];
+
+//CRIPTOGRAFIA DA SENHA ANTES DE SALVAR NO BANCO DE DADOS (por segurança)
+$senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
 ?>
